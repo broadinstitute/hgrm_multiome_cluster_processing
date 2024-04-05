aa5ea2fd02d10f1ee0a7ce8d3fdc51b6421f11f5
Maddie <murphymc98@gmail.com>
2024-04-05 15:41:38 -0400

    Update docker for argparse, upload python file and add wdl calls. First pass - might work

--- cluster_processing/cluster_processing.wdl
+++ cluster_processing/cluster_processing.wdl
@@ -2,6 +2,54 @@ version 1.0
 
 workflow multiome_cluster_processing {
     input {
-        String docker_image = 'us.gcr.io/landerlab-atacseq-200218/hgrm_multiome_cluster_processing:0.1'
+        File expression_h5
+        File atac_fragments_tsv
+        File cluster_labels
+        String docker_image = 'us.gcr.io/landerlab-atacseq-200218/hgrm_multiome_cluster_processing:0.2'
+        String git_branch = "main"
+    }
+
+    call get_cluster_data {
+        input:
+            expression_h5 = expression_h5,
+            atac_fragments_tsv = atac_fragments_tsv,
+            cluster_labels = cluster_labels,
+            docker_image = docker_image,
+            git_branch = git_branch
+    }
+
+    output {
+        Array[File] h5ads = get_cluster_data.h5ads
+        File barcode_level_metadata = get_cluster_data.barcode_level_metadata
+        File cluster_level_metadata = get_cluster_data.cluster_level_metadata
+    }
+}
+
+task get_cluster_data {
+    input {
+        File expression_h5
+        File atac_fragments_tsv
+        File cluster_labels
+        String docker_image
+        String git_branch
+    }
+
+    command {
+        set -ex
+        (git clone https://github.com/broadinstitute/hgrm_multiome_cluster_processing.git /app ; cd /app ; git checkout ${git_branch})
+        micromamba run -n tools2 python3 /app/cluster_processing/multiome_cluster_metadata_and_matrices.py ${expression_h5} ${atac_fragments_tsv} ${cluster_labels}
+    }
+
+    output {
+        Array[File]+ h5ads = glob("*.h5ad")
+        File barcode_level_metadata = "barcode_level_metadata.tsv"
+        File cluster_level_metadata = "cluster_level_metadata.tsv"
+    }
+
+    runtime {
+        docker: docker_image
+        cpu: 1
+        memory: "16GB"
+        preemptible: 1
     }
 }
---
+++ cluster_processing/multiome_cluster_metadata_and_matrices.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import anndata as ad
+import snapatac2 as snap
+import argparse
+
+
+def main():
+    # Read in args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("expression_h5", type=str)
+    parser.add_argument("fragments_tsv", type=str)
+    parser.add_argument("cluster_labels", type=str)
+    args = parser.parse_args()
+
+    print("Reading in scRNA counts.")
+    rna_counts = sc.read_10x_h5(args.expression_h5)
+
+    print("Reading in scATAC fragments. This may take awhile.")
+    atac_counts = snap.pp.import_data(
+        args.fragments.tsv, chrom_sizes=snap.genome.hg38, sorted_by_barcode=False
+    )
+
+    # cluster labels must have cell-barcodes as index, and cluster labels in first column (and likely only column)
+    cell_clusters = pd.read_table(args.cluster_labels, index_col=0)
+
+    print("Generate cell qc metrics.")
+    # mito and ribo info, and total counts, maybe we want users to do this beforehand
+    # but we at least need "total_counts" and "n_fragment" columns in rna, atac, respectively
+    rna_counts.var["mito"] = rna_counts.var_names.str.startswith("MT-")
+    rna_counts.var["ribo"] = rna_counts.var_names.str.startswith(("RPS", "RPL"))
+    sc.pp.calculate_qc_metrics(
+        rna_counts,
+        qc_vars=["mito", "ribo"],
+        percent_top=None,
+        log1p=False,
+        inplace=True,
+    )
+    rna_counts.obs.drop(
+        columns=["total_counts_mito", "total_counts_ribo"], inplace=True
+    )
+
+    rna_counts.obs.columns = "RNA_" + rna_counts.obs.columns
+    atac_counts.obs.columns = "ATAC_" + atac_counts.obs.columns
+    # Info by cell
+    per_cell_metadata = rna_counts.obs.merge(
+        atac_counts.obs, left_index=True, right_index=True, how="outer"
+    )
+
+    # Add cell cluster info to each file (is there a nicer way to do this than 3 lines?)
+    # this matches the file type evie gave, with index = cell names,
+    # and first (and should be only) column corresponding to cluster names, regardless of col name
+    clusters_series = cell_clusters.iloc[:, 0]
+    per_cell_metadata["CellClusterID"] = clusters_series
+    rna_counts.obs["CellClusterID"] = clusters_series
+    atac_counts.obs["CellClusterID"] = clusters_series
+
+    # Info by cluster
+    # atac data needs to have n_fragment col in obs, coming in.
+    per_cluster_metadata = (
+        per_cell_metadata.reset_index()
+        .groupby("CellClusterID")
+        .agg(
+            nCells=("index", "count"),
+            MeanRNAUMIsPerCell=("RNA_total_counts", "mean"),
+            MeanATACFragmentsPerCell=("ATAC_n_fragment", "mean"),
+        )
+    )
+
+    # Get CPM information, normalized within each cluster group
+    for cluster_name in rna_counts.obs.CellClusterID.dropna().unique():
+        tot = rna_counts[rna_counts.obs.CellClusterID == cluster_name, :].X.A.sum(
+            axis=0
+        )
+        cpm = (tot / tot.sum()) * 1e6
+        rna_counts.var[f"{cluster_name}_CPM"] = cpm
+
+    # Run these loops separately to be safe. some clusters may not be in both.
+    print("Saving per-cluster atac files.")
+    for cluster_name in atac_counts.obs.CellClusterID.dropna().unique():
+        atac_cluster_only = atac_counts[
+            atac_counts.obs["CellClusterID"] == cluster_name
+        ]
+        atac_cluster_only.write(f"atac_cluster_{cluster_name}.h5ad", compression="gzip")
+
+    print("Saving per-cluster rna files.")
+    for cluster_name in rna_counts.obs.CellClusterID.dropna().unique():
+        rna_cluster_only = rna_counts[rna_counts.obs["CellClusterID"] == cluster_name]
+        rna_cluster_only.write_h5ad(
+            f"rna_cluster_{cluster_name}.h5ad", compression="gzip"
+        )
+
+    # save metadata tables
+    print("Saving metadata tables.")
+    per_cell_metadata.to_csv("barcode_level_metadata.tsv", sep="\t", header=True)
+    per_cluster_metadata.to_csv("cluster_level_metadata.tsv", sep="\t", header=True)
+
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
