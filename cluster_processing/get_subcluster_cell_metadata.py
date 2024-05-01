772975343eeb37fa27e18ccf38a8822a2fd9c466
Maddie <murphymc98@gmail.com>
2024-05-01 11:35:54 -0400

    concatenate on disk only, dont load into memory

--- cluster_processing/get_subcluster_cell_metadata.py
+++ cluster_processing/get_subcluster_cell_metadata.py
@@ -19,22 +19,29 @@ def main():
     print(f"Reading in all cluster {cluster_name} RNA files.")
     if args.clustered_rna_h5ads is not None:
         if len(args.clustered_rna_h5ads) > 1:
-            all_rna_counts = [sc.read_h5ad(rna) for rna in args.clustered_rna_h5ads]
-            rna_counts = ad.concat(all_rna_counts)
+            # all_rna_counts = [sc.read_h5ad(rna) for rna in args.clustered_rna_h5ads]
+            # concat and write out targ on disk, without loading into memory.
+            print(f"Writing out concatenated rna h5ad for cluster {cluster_name} across inputs.")
+            ad.experimental.concat_on_disk(args.clustered_rna_h5ads, out_file=f'rna_{cluster_name}.h5ad')
+            # read in concatenated counts from disk
+            rna_counts = sc.read_h5ad(f'rna_{cluster_name}.h5ad', backed='r')
         else:
-            rna_counts = sc.read_h5ad(args.clustered_rna_h5ads[0])
-        print(f"Writing out concatenated rna h5ad for cluster {cluster_name} across inputs.")
-        rna_counts.write_h5ad(f'rna_{cluster_name}.h5ad')
+            rna_counts = sc.read_h5ad(args.clustered_rna_h5ads[0], backed='r')
+            print(f"Writing out single rna h5ad for cluster {cluster_name} across inputs.")
+            rna_counts.write_h5ad(f'rna_{cluster_name}.h5ad')
 
     print(f"Reading in all cluster {cluster_name} ATAC files.")
     if args.clustered_atac_h5ads is not None:
         if len(args.clustered_atac_h5ads) > 1:
-            all_atac_counts = [sc.read_h5ad(atac) for atac in args.clustered_atac_h5ads]
-            atac_counts = ad.concat(all_atac_counts)
+            #all_atac_counts = [sc.read_h5ad(atac) for atac in args.clustered_atac_h5ads]
+            print(f"Writing out concatenated atac h5ad for cluster {cluster_name} across inputs.")
+            ad.experimental.concat_on_disk(args.clustered_atac_h5ads, out_file=f'atac_{cluster_name}.h5ad')
+            # read in concatenated counts from disk
+            atac_counts = sc.read_h5ad(f'atac_{cluster_name}.h5ad', backed='r')
         else:
-            atac_counts = sc.read_h5ad(args.clustered_atac_h5ads[0])
-        print(f"Writing out concatenated atac h5ad for cluster {cluster_name} across inputs.")
-        atac_counts.write_h5ad(f'atac_{cluster_name}.h5ad')
+            atac_counts = sc.read_h5ad(args.clustered_atac_h5ads[0], backed='r')
+            print(f"Writing out single atac h5ad for cluster {cluster_name} across inputs.")
+            atac_counts.write_h5ad(f'atac_{cluster_name}.h5ad')
 
 
     # read in cell cluster info and get in same format as cluster names
